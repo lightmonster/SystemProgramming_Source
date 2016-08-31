1535a4030b66149375653824d618b48e1d5b3074
cs241svngrader <cs241svngrader@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-08-31 19:33:49 +0000

    code examples

---
+++ lecture_code/05-powerful.c
@@ -0,0 +1,9 @@
+// Lawrence Angrave CS241 Lecture Demo
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char**argv) {
+   printf("Executing %s ...\n", argv[1] ); // same as *(argv+1) );
+   execvp( argv[1] , argv + 1);
+   perror("Failed to be all powerful");
+}
