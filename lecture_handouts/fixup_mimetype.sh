285cf8f7cb90c4b86448cb07e00bcdbe8878c12b
angrave <angrave@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-09 15:31:07 +0000

    fixup_mimetype.sh

---
+++ lecture_handouts/fixup_mimetype.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+svn propset svn:mime-type 'application/pdf' CS241FA16*.pdf
+svn propset svn:mime-type 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' CS241FA16*docx
