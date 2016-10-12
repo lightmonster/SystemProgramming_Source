03ad79e9558324455ce73e126f55a885097f91f6
angrave <angrave@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-10-12 19:09:33 +0000

    23

--- lecture_code/23/23_half_solution.c
+++ lecture_code/23/23_half_solution.c
@@ -2,5 +2,13 @@
 // Hint fopen, fseek, ftell, fread, malloc, fclose
 
 char* half(char*filename) {
-
+  FILE *f = fopen(filename,"r");
+  fseek( f, 0, SEEK_END);
+  long halfsize = ftell(f)/2;
+  char* result = malloc( halfsize + 1);
+  result[halfsize] = '\0';
+  rewind(f); //or fseek(f, 0, SEEK_SET);
+  fread( result, 1, halfsize, f);
+  fclose(f);
+  return result;
 }
--- lecture_code/23/23_rewind_solution.c
+++ lecture_code/23/23_rewind_solution.c
@@ -8,13 +8,22 @@ typedef struct _FILE {
 } FILE;
 // hint  fseek or fflush and lseek are useful here
 void rewind(FILE*f) {
+  fflush(f);
+  lseek( f->fd, 0 , SEEK_SET);
 
-
+  //or,
+  fseek( f, 0, SEEK_SET); 
 }
 
 //hint: use and reset the buffer content
 // Use write
 void fflush(FILE*f) {
-
+  write(f->fd, f->buffer,  f->size);
+  f->size = 0;
 }
 
+void fclose(FILE*f) {
+  fflush(f);
+  close(f->fd);
+  // free(buffer) too if we created it
+}
