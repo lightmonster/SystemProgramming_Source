0c0c8079409200ec00036f4fd9f8583fa4fe98a6
angrave <angrave@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-11-09 21:46:31 +0000

    35

--- lecture_code/35/bitpuzzle.c
+++ lecture_code/35/bitpuzzle.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+// also try with ~16
 
 int main() {
-  int i = ~ 1;
+  int i = ~ 0;
   printf("%d  %x\n", i,i);
   return 1;
 }
--- lecture_code/35/noint.c
+++ lecture_code/35/noint.c
@@ -1,12 +1,17 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
-
 #include <unistd.h>
+
+// Prep: gcc takeovertheworld.c -o takeovertheworld
+// Demo: ./a.out ./takeovertheworld 13 
+// also discuss nohup command &
+
 void quit(char*mesg) { perror(mesg); exit(1); }
 
 int main(int argc, char**argv) {
     if(argc <2) {fprintf(stderr,"Usage: %s command args\n", argv[0]); exit(1);}
+    puts("Press CTRL-C as much as you want...");
 
     sigset_t mask;
     sigset_t orig_mask;
