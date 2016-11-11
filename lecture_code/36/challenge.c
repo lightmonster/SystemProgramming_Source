f7389de9213e62cad37e4a5e2a4311fa53976d95
angrave <angrave@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-11-11 20:12:51 +0000

    36

---
+++ lecture_code/36/challenge.c
@@ -0,0 +1,16 @@
+//Change ONE character to fix this program to print 20 dashes. 
+
+#include <stdio.h>
+int main()
+{
+  int a;
+  int b = 20;
+  for( a = 0; a < b; a-- )
+    putchar('-');
+  return 0;
+}
+
+//Actually there are THREE solutions (again, changing only ONE character). Find all three.
+
+//Bonus: Change one character to print 21 dashes.
+
